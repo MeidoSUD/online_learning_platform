3a1e765bc2461d6561b679faf242763ec68a6ded
muhammed2020osman <muhammed2020osman@gmail.com>
2026-02-09 08:49:48 +0200

    teacherWalletService done

--- app/Models/WalletTransaction.php
+++ app/Models/WalletTransaction.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class WalletTransaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'wallet_id',
+        'type',
+        'amount',
+        'balance_before',
+        'balance_after',
+        'description',
+        'meta'
+    ];
+
+    protected $casts = [
+        'meta' => 'array'
+    ];
 }
--- note/sql.sql
+++ note/sql.sql
@@ -6,3 +6,9 @@ ALTER TABLE `sessions` ADD `session_title` VARCHAR(222) NULL DEFAULT NULL AFTER 
 
 6-Feb
 ALTER TABLE `reviews` ADD `session_id` INT NULL DEFAULT NULL AFTER `course_id`;
+9-Feb
+
+ALTER TABLE `sessions` CHANGE `status` `status` ENUM('scheduled','in_progress','completed','cancelled','no_show','live','ended') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+ALTER TABLE `wallet_transactions` ADD `meta` VARCHAR(255) NULL DEFAULT NULL AFTER `amount`;
+ALTER TABLE `wallet_transactions` ADD `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_at`;
