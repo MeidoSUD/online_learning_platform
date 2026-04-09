5bc8c230f3a063d228cb7d6761b358ddb4c1bc08
meido <meido3123@gmail.com>
2026-04-09 07:42:45 +0300

    update services to return image path

--- app/Http/Controllers/API/ServicesController.php
+++ app/Http/Controllers/API/ServicesController.php
@@ -17,7 +17,7 @@ class ServicesController extends Controller
 
     public function listServices()
     {
-        $services = \App\Models\Services::where('status', 1)->get(['key_name', 'name_en', 'name_ar', 'description_en', 'description_ar', 'id']);
+        $services = \App\Models\Services::where('status', 1)->get(['key_name', 'name_en', 'name_ar', 'description_en', 'image','description_ar', 'id']);
         return response()->json($services);
     }
 
